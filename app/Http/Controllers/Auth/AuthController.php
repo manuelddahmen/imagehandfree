0480dbd0ab6ac2d051caaea0562c088ab3040ace
Manuel Dahmen <38113629+manuelddahmen@users.noreply.github.com>
2020-10-27 10:19:25 +0100

    Update AuthController.php

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -24,12 +24,10 @@ class AuthController extends Controller
     | authentication of existing users. By default, this controller uses
     | a simple trait to add these behaviors. Why don't you explore it?
     |
-    *//*
-	use /*AuthenticatesUsers, RegistersUsers {
-		AuthenticatesUsers::redirectPath insteadof RegistersUsers;
-		AuthenticatesUsers::guard insteadof RegistersUsers;
-	}*/
-use RegistersUsers;
+    */
+	use AuthenticatesUsers, RegistersUsers;
+	}
+	
 	protected function redirectTo()
 
     {
